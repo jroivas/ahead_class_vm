7b34c073f8792744959ed9a80d1bae9e0ca64d5b
Jouni Roivas <jroivas@iki.fi>
2016-09-22 01:37:45 +0300

    Verbosal code

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,25 @@ int main(int argc, char **argv)
     std::cout << "Fields " << m.fields_count << "\n";
     std::cout << "Methods " << m.methods_count << "\n";
     std::cout << "Attrib " << m.attributes_count << "\n";
+    for (auto me : m.methods) {
+        uint16_t i = 1;
+        vmConstantUtf8 *mu = dynamic_cast<vmConstantUtf8*>(m.constant_pool[me->name_index]);
+        if (mu) {
+            std::cout << "Method: " << mu->bytes << "\n";
+        }
+        for (auto a : me->attributes) {
+            vmConstantUtf8 *u = dynamic_cast<vmConstantUtf8*>(m.constant_pool[a->name_index]);
+            if (u != nullptr) {
+                std::cout << "Section: " << u->bytes << "\n";
+                //std::cout << i << " " << u->bytes << "\n";
+                for (uint32_t l = 0; l< a->length; ++l) {
+                    std::cout << " " << (unsigned int)a->info[l] << " ";
+                }
+                std::cout << "\n";
+            }
+            ++i;
+        }
+    }
 
     return 0;
 }
